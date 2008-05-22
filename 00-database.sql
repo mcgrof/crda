e87705cb7b713d95c673704d563b7e4486aface9
Johannes Berg <johannes@sipsolutions.net>
2008-05-22 13:19:09 +0200

    roll permissions into database setup sql

--- 00-database.sql
+++ 00-database.sql
@@ -1,3 +1,7 @@
 /* create database */
 
 create database if not exists regulatory;
+
+/* add permissions */
+use regulatory;
+grant all privileges on regulatory.* to admin@localhost identified by '802.11r';
--- 07-user-and-permissions.sql
+++
@@ -1,2 +0,0 @@
-use regulatory;
-grant all privileges on regulatory.* to admin@localhost identified by '802.11r';

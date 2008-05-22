26edda29d17c9c016c4de0e2b828433c4d3d43ac
Johannes Berg <johannes@sipsolutions.net>
2008-05-22 14:52:57 +0200

    sanity-check file offsets

--- daemon/dump.c
+++ daemon/dump.c
@@ -9,13 +9,27 @@
 
 #include "regdb.h"
 
-static void print_reg_rule(__u8 *db, struct regdb_file_reg_rule *rule)
+static void *get_file_ptr(__u8 *db, int dblen, int structlen, __be32 ptr)
 {
+	__u32 p = ntohl(ptr);
+
+	if (p > dblen - structlen) {
+		fprintf(stderr, "Invalid database file, bad pointer!\n");
+		exit(3);
+	}
+
+	return (void *)(db + p);
+}
+
+static void print_reg_rule(__u8 *db, int dblen, __be32 ruleptr)
+{
+	struct regdb_file_reg_rule *rule;
 	struct regdb_file_freq_range *freq;
 	struct regdb_file_power_rule *power;
 
-	freq = (void *)(db + ntohl(rule->freq_range_ptr));
-	power = (void *)(db + ntohl(rule->power_rule_ptr));
+	rule = get_file_ptr(db, dblen, sizeof(*rule), ruleptr);
+	freq = get_file_ptr(db, dblen, sizeof(*freq), rule->freq_range_ptr);
+	power = get_file_ptr(db, dblen, sizeof(*power), rule->power_rule_ptr);
 
 	printf("\t%d..%d kHz (maxbw: %d kHz, mod: 0x%x, restrict: 0x%x)\n",
 	       ntohl(freq->start_freq),
@@ -40,7 +54,7 @@ int main(int argc, char **argv)
 	__u8 *db;
 	struct regdb_file_header *header;
 	struct regdb_file_reg_country *countries;
-	int num_countries, i, j;
+	int dblen, num_countries, i, j;
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
@@ -58,15 +72,15 @@ int main(int argc, char **argv)
 		return 2;
 	}
 
-	/* XXX: check file length to be at least >= sizeof(struct regdb_file_header) */
+	dblen = stat.st_size;
 
-	db = mmap(NULL, stat.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	db = mmap(NULL, dblen, PROT_READ, MAP_SHARED, fd, 0);
 	if (db == MAP_FAILED) {
 		perror("failed to mmap db file");
 		return 2;
 	}
 
-	header = (void *)db;
+	header = get_file_ptr(db, dblen, sizeof(*header), 0);
 
 	if (ntohl(header->magic) != REGDB_MAGIC) {
 		fprintf(stderr, "Invalid database magic\n");
@@ -79,18 +93,24 @@ int main(int argc, char **argv)
 	}
 
 	num_countries = ntohl(header->reg_country_num);
-	countries = (void *)(db + ntohl(header->reg_country_ptr));
-
-	/* XXX: check countries pointer to be in file */
+	countries = get_file_ptr(db, dblen,
+				 sizeof(struct regdb_file_reg_country) * num_countries,
+				 header->reg_country_ptr);
 
 	for (i = 0; i < num_countries; i++) {
 		struct regdb_file_reg_rules_collection *rcoll;
 		struct regdb_file_reg_country *country = countries + i;
+		int num_rules;
 
 		printf("Country %.2s\n", country->alpha2);
-		rcoll = (void *)db + ntohl(country->reg_collection_ptr);
-		for (j = 0; j < ntohl(rcoll->reg_rule_num); j++)
-			print_reg_rule(db, (void *)(db + ntohl(rcoll->reg_rule_ptrs[j])));
+		rcoll = get_file_ptr(db, dblen, sizeof(*rcoll), country->reg_collection_ptr);
+		num_rules = ntohl(rcoll->reg_rule_num);
+		/* re-get pointer with sanity checking for num_rules */
+		rcoll = get_file_ptr(db, dblen,
+				     sizeof(*rcoll) + num_rules * sizeof(__be32),
+				     country->reg_collection_ptr);
+		for (j = 0; j < num_rules; j++)
+			print_reg_rule(db, dblen, rcoll->reg_rule_ptrs[j]);
 	}
 
 	return 0;

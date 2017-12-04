067d961a344b4a2b2c374cfba5a632f0652971e2
Jelle van der Waa <jelle@vdwaa.nl>
2017-12-04 21:52:39 +0100

    crda: Fix error: `keys’ defined but not used
    Fix error: ‘keys’ defined but not used [-Werror=unused-const-variable=]
    by informing GCC keys is unused.
    
    Signed-off-by: Jelle van der Waa <jelle@vdwaa.nl>
    Signed-off-by: Luis R. Rodriguez <mcgrof@kernel.org>

--- utils/key2pub.py
+++ utils/key2pub.py
@@ -115,7 +115,7 @@ struct key_params {
 	.n = _n, .len_n = sizeof(_n),	\
 }
 
-static const struct key_params keys[] = {
+static const struct key_params __attribute__ ((unused)) keys[] = {
 ''')
     for n in xrange(n + 1):
         output.write('	KEYS(e_%d, n_%d),\n' % (n, n))
